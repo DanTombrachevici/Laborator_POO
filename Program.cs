e02fe2ebee723ac10e8448298ac94b71424862ef
DanTombrachevici <75080623+DanTombrachevici@users.noreply.github.com>
2021-10-23 21:18:06 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,121 +6,64 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Monitor
+    class Text
     {
-        public string color;
-        public int dimension;
-        public int res_width;
-        public int res_height;
+        public string text1;
+
+        public void Read()
+        {
+            text1 = System.IO.File.ReadAllText("Textul.txt");
+            System.Console.WriteLine("Textul :\n{0}\n",text1);
+        }
+
+        public void number_s()
+        {
+            string[] sent = text1.Split('.');
+            int sentences = sent.Length;
+            Console.WriteLine("Numarul de propozitii {0}", sentences);
+        }
+
+        public void number_w()
+        {
+            string[] word = text1.Split(' ');
+            int w = word.Length;
+            Console.WriteLine("Numarul de cuvinte {0}", w);
+        }
+        public void number_l()
+        {
+            int vocale = 0;
+            int consoane = 0;
+
+            for (int i = 0; i < text1.Length; i++)
+            {
+                if (text1[i] == 'a' || text1[i] == 'e' || text1[i] == 'i' || text1[i] == 'o' || text1[i] == 'u' || text1[i] == 'A' || text1[i] == 'E' || text1[i] == 'I' || text1[i] == 'O' || text1[i] == 'U')
+                    vocale++;
+                else
+                    consoane++;
+            }
+            int litere = vocale + consoane;
+
+            Console.WriteLine("Numarul de vocale {0}", vocale);
+            Console.WriteLine("Numarul de consoane {0}", consoane);
+            Console.WriteLine("Numarul de litere {0}", litere);
+
+        }
+
 
     }
-  
 
     class Program
     {
-        static Monitor GetMonitor()
-        {
-            var Monitor1 = new Monitor();
-            Console.WriteLine("Introduceti culoarea monitorului");
-            Monitor1.color = Console.ReadLine();
-            Console.WriteLine("Introduceti dimensiunea monitorului");
-            Monitor1.dimension = int.Parse(Console.ReadLine());
-            Console.WriteLine("Introduceti width monitorului");
-            Monitor1.res_width = int.Parse(Console.ReadLine());
-            Console.WriteLine("Introduceti height monitorului");
-            Monitor1.res_height = int.Parse(Console.ReadLine());
-            return Monitor1;
-        }
-        static Monitor GetMonitor2()
-        {
-            var Monitor2 = new Monitor();
-            Monitor2.color = "black";
-            Monitor2.dimension = 27;
-            Monitor2.res_width = 1920;
-            Monitor2.res_height = 1080;
-            return Monitor2;
-        }
-
-        static void Print(Monitor Monitor1)
-        {
-            Console.WriteLine("#############################################");
-            Console.WriteLine("Informatia despre primul monitor:");
-            Console.WriteLine($"Color : {Monitor1.color}");
-            Console.WriteLine($"Dimension : {Monitor1.dimension}cm");
-            Console.WriteLine($"Resolution : {Monitor1.res_width}x{Monitor1.res_height}");
-            Console.WriteLine("#############################################");
-        }
-        static void Print2(Monitor Monitor2)
-        {
-            Console.WriteLine("#############################################");
-            Console.WriteLine("Informatia despre monitorul 2:");
-            Console.WriteLine($"Color : {Monitor2.color}");
-            Console.WriteLine($"Dimension : {Monitor2.dimension}cm");
-            Console.WriteLine($"Resolution : {Monitor2.res_width}x{Monitor2.res_height}");
-            Console.WriteLine("#############################################");
-        }
-        static void PrintMenu()
-        {
-            Console.WriteLine("--------------------Menu--------------------");
-            Console.WriteLine("1 - Schimba parametrii primului monitor");
-            Console.WriteLine("2 - Printeaza informatia despre primul monitor");
-            Console.WriteLine("3 - Printeaza informatia despre monitorul 2");
-            Console.WriteLine("4 - Compara monitoarele");
-            Console.WriteLine("5 - Clean Console");
-            Console.WriteLine("0 - EXIT");
-            Console.WriteLine("--------------------Menu--------------------");
-        }
-        static void Compare(Monitor Monitor1 , Monitor Monitor2)
-        {
-            Console.WriteLine("#############################################");
-            if (Monitor1.color == Monitor2.color)
-                Console.WriteLine("Culorile sunt la fel");
-            else 
-                Console.WriteLine("Culorile nu sunt la fel");
-            if(Monitor1.dimension == Monitor2.dimension)
-                Console.WriteLine("Dimensiunele sunt egale");
-            else
-                Console.WriteLine("Dimensiunele nu sunt egale");
-            if(Monitor1.res_width == Monitor2.res_width && Monitor1.res_height == Monitor2.res_height)
-                Console.WriteLine("Resolutiile sunt la fel");
-            else
-                Console.WriteLine("Resolutiile nu sunt la fel");
-            Console.WriteLine("#############################################");
-        }
-
-
         static void Main(string[] args)
         {
-            var firstMonitor = GetMonitor();
-            var secondMonitor = GetMonitor2();
-            PrintMenu();
-            var x = int.Parse(Console.ReadLine());
-            do
-            { 
-            switch (x)
-                {
-                    case 1:
-                        firstMonitor = GetMonitor();
-                        PrintMenu();
-                        break;
-                    case 2:
-                        Print(firstMonitor);
-                        PrintMenu();
-                        break;
-                    case 3:
-                        Print2(secondMonitor);
-                        break;
-                    case 4:
-                        Compare(firstMonitor, secondMonitor);
-                        break;
-                    case 5:
-                        Console.Clear();
-                        PrintMenu();
-                        break;
-                }
-                x = int.Parse(Console.ReadLine());
-            } while (x > 0);
 
-    }
+            Text text = new Text();
+            text.Read();
+            text.number_s();
+            text.number_w();
+            text.number_l();
+
+
+        }
     }
 }
